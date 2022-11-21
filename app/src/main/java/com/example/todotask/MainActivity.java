c8eef693a55eaa97e7773f236c5c1eab07ddd9ae
eesuhn <102596628+eesuhn@users.noreply.github.com>
2022-11-21 21:22:36 +0800

    Added popupMenu, NoteEditor

--- app/src/main/java/com/example/todotask/MainActivity.java
+++ app/src/main/java/com/example/todotask/MainActivity.java
@@ -2,19 +2,69 @@ package com.example.todotask;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.ContextThemeWrapper;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     static ArrayList<String> task = new ArrayList<>();
-    static ArrayAdapter arrayAdapter;
+    static ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        //menu
+        FloatingActionButton menu = findViewById(R.id.menu);
+        menu.setOnClickListener(view -> {
+            Context wrapper = new ContextThemeWrapper(MainActivity.this, R.style.popupMenu);
+            PopupMenu popupMenu = new PopupMenu(wrapper, menu);
+            popupMenu.getMenuInflater().inflate(R.menu.popup_menu, popupMenu.getMenu());
+            popupMenu.setOnMenuItemClickListener(menuItem -> {
+                if (menuItem.getTitle() == getString(R.string.newTask)){
+                    Intent intent = new Intent(getApplicationContext(), NoteEditor.class);
+                    startActivity(intent);
+                }
+                if (menuItem.getTitle() == getString(R.string.aboutUs)){
+                    Toast.makeText(MainActivity.this, "Working in Progress..", Toast.LENGTH_SHORT).show(); //about us
+                }
+                return true;
+            });
+            popupMenu.show();
+        });
+        //Menu
+        ListView listView = findViewById(R.id.listView);
+
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences(".todotask", Context.MODE_PRIVATE);
+
+        HashSet<String> set = (HashSet<String>) sharedPreferences.getStringSet("task", null);
+
+        if (set == null){
+            task.add(getString(R.string.newTask));
+        }else{
+            task = new ArrayList<>(set);
+        }
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, task);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener((adapterView, view, i, l) -> {
+            Intent intent = new Intent(getApplicationContext(), NoteEditor.class);
+            intent.putExtra("noteID", i);
+            startActivity(intent);
+        });
     }
 }
---
+++ app/src/main/java/com/example/todotask/NoteEditor.java
@@ -0,0 +1,58 @@
+package com.example.todotask;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.HashSet;
+import java.util.Objects;
+
+public class NoteEditor extends AppCompatActivity {
+
+    int noteID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.note_editor);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        EditText editText = findViewById(R.id.editTask);
+
+        Intent intent = getIntent();
+        noteID = intent.getIntExtra("noteID", -1);
+
+        if (noteID != -1){
+            editText.setText(MainActivity.task.get(noteID));
+        }else{
+            MainActivity.task.add("");
+            noteID = MainActivity.task.size() - 1;
+            MainActivity.arrayAdapter.notifyDataSetChanged();
+        }
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                MainActivity.task.set(noteID, String.valueOf(charSequence));
+                MainActivity.arrayAdapter.notifyDataSetChanged();
+
+                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences(".todotask", Context.MODE_PRIVATE);
+
+                HashSet<String> set = new HashSet<>(MainActivity.task);
+
+                sharedPreferences.edit().putStringSet("task", set).apply();
+            }
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+    }
+}
